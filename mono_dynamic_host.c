99a7c67662c064e5108c398d7d52a7e797ee2693
DaNike <danike@cirr.com>
2024-05-22 05:51:20 -0500

    Fix silly errors

--- mono_dynamic_host.c
+++ mono_dynamic_host.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-#   define MDH_WINDOWS
-#   ifdef _WIN64
-#		define BITS64
+# define MDH_WINDOWS
+# ifdef _WIN64
+#   define BITS64
 #	endif	
 #elif __APPLE__
-#	define MDH_OSX
+# define MDH_OSX
 #elif defined(_POSIX_VERSION)
 #	define MDH_POSIX
 #else
@@ -32,79 +32,79 @@ typedef int (*mono_jit_exec_t)(MonoDomain *domain, MonoAssembly *assembly, int a
 
 int main(int argc, char **argv)
 {
-	// We always require at least 2 arguments
-    if(argc < 3)
-	{
-        printf("usage: %s <mono dynamic library> <.NET executable> [<arg> ...]\n", argv[0]);
-        return 1;
+  // We always require at least 2 arguments
+  if(argc < 3)
+  {
+    printf("usage: %s <mono dynamic library> <.NET executable> [<arg> ...]\n", argv[0]);
+    return 1;
+  }
+  
+  #if defined(MDH_LIBDL)
+
+    // always clear dlerror() just to be sure
+    dlerror();
+  
+    void *monoHnd = dlopen(argv[1], RTLD_LAZY | RTLD_GLOBAL);
+    if (!monoHnd)
+    {
+      fprintf(stderr, "mdh: could not load mono from %s: %s\n", argv[1], dlerror());
+      return 1;
     }
-	
-#ifdef MDH_LIBDL
-	// always clear dlerror() just to be sure
-	dlerror();
-	
-	#if 
-	void *monoHnd = dlopen(argv[1], RTLD_LAZY | RTLD_GLOBAL);
-	if (!monoHnd)
-	{
-		fprintf(stderr, "mdh: could not load mono from %s: %s\n", argv[1], dlerror());
-		return 1;
-	}
-	
-	#define LOAD_SYM(name) \
-		name##_t name = dlsym(monoHnd, #name); \
-		if (!name) \
-		{ \
-			fprintf(stderr, "mdh: could not load symbol '%s': %s\n", #name, dlerror()); \
-			return 1; \
-		}
-	
-#elif defined(MDH_WINDOWS)
+  
+    #define LOAD_SYM(name) \
+      name##_t name = (name##_t)dlsym(monoHnd, #name); \
+      if (!name) \
+      { \
+        fprintf(stderr, "mdh: could not load symbol '%s': %s\n", #name, dlerror()); \
+        return 1; \
+      }
+  
+  #elif defined(MDH_WINDOWS)
 
-	HMODULE monoHnd = LoadLibraryA(argv[1]);
-	if (!monoHnd)
-	{
-		DWORD error = GetLastError();
-		LPSTR message;
-		if (!FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-				NULL, error, 0, (LPSTR)&message, 0, NULL))
-		{
-			fprintf(stderr, "mdh: could not format error code %#x: %#x\r\n", error, GetLastError());
-			return 1;
-		}
-		
-		fprintf(stderr, "mdh: could not load mono from %s: %s\r\n", argv[1], message);
-		return 1;
-	}
-	
-	#define LOAD_SYM(name) \
-		name##_t name = GetProcAddress(monoHnd, #name); \
-		if (!name) \
-		{ \
-			DWORD error = GetLastError(); \
-			LPSTR message; \
-			if (!FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, \
-					NULL, error, 0, (LPSTR)&message, 0, NULL)) \
-			{ \
-				fprintf(stderr, "mdh: could not format error code %#x: %#x\r\n", error, GetLastError()); \
-				return 1; \
-			} \
-			\
-			fprintf(stderr, "mdh: could not load symbol '%s': %s\r\n", #name, message); \
-			return 1; \
-		}
+    HMODULE monoHnd = LoadLibraryA(argv[1]);
+    if (!monoHnd)
+    {
+      DWORD error = GetLastError();
+      LPSTR message;
+      if (!FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+                          NULL, error, 0, (LPSTR)&message, 0, NULL))
+      {
+        fprintf(stderr, "mdh: could not format error code %#x: %#x\r\n", error, GetLastError());
+        return 1;
+      }
+    
+      fprintf(stderr, "mdh: could not load mono from %s: %s\r\n", argv[1], message);
+      return 1;
+    }
+  
+    #define LOAD_SYM(name) \
+      name##_t name = (name##_t)GetProcAddress(monoHnd, #name); \
+      if (!name) \
+      { \
+        DWORD error = GetLastError(); \
+        LPSTR message; \
+        if (!FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, \
+                            NULL, error, 0, (LPSTR)&message, 0, NULL)) \
+        { \
+          fprintf(stderr, "mdh: could not format error code %#x: %#x\r\n", error, GetLastError()); \
+          return 1; \
+        } \
+        \
+        fprintf(stderr, "mdh: could not load symbol '%s': %s\r\n", #name, message); \
+        return 1; \
+      }
 
-#else
-	#error "Unknown dynamic loader"
-#endif
-	
-	LOAD_SYM(mono_jit_init);
-	LOAD_SYM(mono_domain_assembly_open);
-	LOAD_SYM(mono_jit_exec);
-	
-	// Now that we've loaded Mono, lets execute
-	
-	MonoDomain *domain = mono_jit_init(argv[2]);
-	MonoAssembly *assembly = mono_domain_assembly_open(domain, argv[2]);
-    return mono_jit_exec(domain, assembly, argc - 2, argv + 2);
+  #else
+    #error "Unknown dynamic loader"
+  #endif
+  
+  LOAD_SYM(mono_jit_init);
+  LOAD_SYM(mono_domain_assembly_open);
+  LOAD_SYM(mono_jit_exec);
+  
+  // Now that we've loaded Mono, lets execute
+  
+  MonoDomain *domain = mono_jit_init(argv[2]);
+  MonoAssembly *assembly = mono_domain_assembly_open(domain, argv[2]);
+  return mono_jit_exec(domain, assembly, argc - 2, argv + 2);
 }
