e502972fe0bc1ad79c738b3e1d1ffe01a4864ec2
DaNike <danike@cirr.com>
2024-06-14 22:39:19 -0500

    Begin work to properly support .NET Mono

--- mono_dynamic_host.c
+++ mono_dynamic_host.c
@@ -18,12 +18,16 @@
 
 #ifdef MDH_WINDOWS
 # define WIN32_LEAN_AND_MEAN
-#	include <windows.h>
+# include <windows.h>
 #endif
 
 #if defined(MDH_POSIX) || defined(MDH_OSX)
-#	define MDH_LIBDL
-#	include <dlfcn.h>
+# define MDH_LIBDL
+# include <dlfcn.h>
+# include <libgen.h> // dirname
+# include <sys/types.h> // stat
+# include <sys/stat.h> // stat
+# include <unistd.h> // stat
 #endif
 
 // ---- PAL ----
@@ -81,6 +85,40 @@ static void PAL_ReportError(char const* message, ...)
   fprintf(stderr, "mdh: %s: %s\n", buffer, error);
 
   free(error);
+}
+
+static char* PAL_Dirname(char const* path)
+{
+  // on *nix, we'll use dirname, but havet o do some funky stuff to get it to behave
+  char* dupPath = PAL_StrDup(path);
+  char* dir = dirname(dupPath);
+
+  size_t len = strlen(dir);
+  int addSlash = 0;
+  if (len && dir[len - 1] != '/')
+  {
+    addSlash = 1;
+  }
+
+  char* result = malloc(len + addSlash + 1);
+  result[0] = 0;
+  strcpy(result, dir);
+  if (addSlash)
+  {
+    result[len] = '/';
+    result[len + 1] = 0;
+  }
+  // note: dir cannot be freed; it may be in the same alloc as dupPath though
+  free(dupPath);
+  return result;
+}
+
+static int PAL_FileExists(char const* path)
+{
+  struct stat statbuf;
+
+  if (stat(path, &statbuf) == -1) return 0;
+  return (statbuf.st_mode & S_IFMT) == S_IFREG;
 }
 
 #elif defined(MDH_WINDOWS)
@@ -147,6 +185,46 @@ static void PAL_ReportError(char const* message, ...)
   free(buffer);
 }
 
+static char* PAL_Dirname(char const* path)
+{
+  // on Windows, we'll use _splitpath_s
+
+  size_t pathLen = strlen(path);
+  char driveBuffer[4] = { 0 };
+  char* dirBuffer = malloc(pathLen + 1);
+
+  errno_t result = _splitpath_s(path,
+    driveBuffer, sizeof(driveBuffer) / sizeof(driveBuffer[0]),
+    dirBuffer, pathLen,
+    NULL, 0,  // filename
+    NULL, 0); // extension
+
+  if (result != 0)
+  {
+    PAL_ReportError("PAL_Dirname(%s): _splitpath_s() returned %d", path, result);
+    exit(1);
+  }
+
+  // now we have the split, lets merge the bits we actually care about back together
+  char* resultBuffer = malloc(pathLen + 1);
+  resultBuffer[0] = 0;
+  strcat(resultBuffer, driveBuffer);
+  strcat(resultBuffer, dirBuffer);
+  free(dirBuffer);
+
+  // result includes trailing slash
+  return resultBuffer;
+}
+
+static int PAL_FileExists(char const* path)
+{
+  DWORD attr = GetFileAttributesA(path);
+
+  // file exists, and is not a directory
+  return attr != INVALID_FILE_ATTRIBUTES
+    && (attr & FILE_ATTRIBUTE_DIRECTORY) == 0;
+}
+
 #else
 #error "Unknown dynamic loader"
 #endif
@@ -160,8 +238,13 @@ typedef enum {
   MONO_DEBUG_FORMAT_DEBUGGER, // <-- Deprecated
 } MonoDebugFormat;
 
+// Main Mono entrypoint
 typedef int (*mono_main_t)(int argc, char* argv[]);
 
+// dotnet/runtime Mono runtime initialization
+typedef int (*monovm_initialize_t)(int propertyCount, char const** propertyKeys, char const** propertyValues);
+
+// Manual hosting APIs
 typedef MonoDomain* (*mono_jit_init_t)(char const* file);
 typedef MonoAssembly* (*mono_domain_assembly_open_t)(MonoDomain* domain, char const* name);
 typedef void (*mono_assembly_setrootdir_t)(char const* root_dir);
@@ -206,6 +289,100 @@ static void print_usage(char const* name, int includeHelp)
   }
 }
 
+static char* probe_corlib_path(char const* monoDllDirname, char const* corlibName)
+{
+  size_t dirnameLen = strlen(monoDllDirname);
+  size_t corlibNameLen = strlen(corlibName);
+
+  // dirname always includes trailing slash, so we can just shove together the bits of the filename
+  char* testPath = malloc(dirnameLen + corlibName + 4 + 1);
+  strcpy(testPath, monoDllDirname);
+  strcpy(testPath + dirnameLen, corlibName);
+  strcpy(testPath + dirnameLen + corlibNameLen, ".dll");
+
+  if (PAL_FileExists(testPath))
+  {
+    // if the file exists, return the constructed path
+    return testPath;
+  }
+  else
+  {
+    // otherwise, free it and return null
+    free(testPath);
+    return NULL;
+  }
+}
+
+#define LOAD_SYM_OPT(name) \
+  name##_t name = (name##_t)PAL_GetSymbol(monoHnd, #name);
+#define LOAD_SYM(name) \
+  LOAD_SYM_OPT(name) \
+  if (!name) \
+  { \
+    PAL_ReportError("could not load symbol '%s'", #name); \
+    return 1; \
+  }
+
+typedef enum
+{
+  MONOVM_Success,
+  MONOVM_NotMonoVM,
+  MONOVM_Error,
+} MonoVMInitResult;
+
+static MonoVMInitResult try_init_monovm(char const* monoDll, void* monoHnd)
+{
+  LOAD_SYM_OPT(monovm_initialize);
+  if (!monovm_initialize) return MONOVM_NotMonoVM;
+
+  // this is MonoVM, we need to give it TRUSTED_PLATFORM_ASSEMBLIES to be able to load its corlib.
+  // We'll look in some env vars first, then probe next to the Mono binary itself.
+
+  char* dirname = NULL;
+  char* corlib = NULL;
+
+  // first try MDH-specific env var
+  corlib = PAL_DupEnv("MDH_TRUSTED_PLATFORM_ASSEMBLIES");
+  if (!corlib)
+  {
+    // then try a general env var
+    corlib = PAL_DupEnv("TRUSTED_PLATFORM_ASSEMBLIES");
+  }
+  // if we haven't gotten an env var, probe for corlibs
+  if (!corlib)
+  {
+    dirname = PAL_Dirname(monoDll);
+    corlib = probe_corlib_path(dirname, "System.Private.CoreLib");
+  }
+  if (!corlib)
+  {
+    corlib = probe_corlib_path(dirname, "mscorlib");
+  }
+
+  free(dirname);
+
+  // only pass in the prop if we found a corlib during probing
+  int numProperties = corlib ? 1 : 0;
+
+  const char* propertyKeys[] = {
+    "TRUSTED_PLATFORM_ASSEMBLIES"
+  };
+
+  const char* propertyValues[] = {
+    corlib
+  };
+
+  int result = monovm_initialize(numProperties, propertyKeys, propertyValues);
+  free(corlib);
+  if (result != 0)
+  {
+    PAL_ReportError("monovm_initialize() returned %#lx", result);
+    return MONOVM_Error;
+  }
+
+  return MONOVM_Success;
+}
+
 int main(int argc, char **argv)
 {
   if (argc < 2)
@@ -229,18 +406,14 @@ int main(int argc, char **argv)
     return 1;
   }
 
-#define LOAD_SYM_OPT(name) \
-  name##_t name = (name##_t)PAL_GetSymbol(monoHnd, #name);
-#define LOAD_SYM(name) \
-  LOAD_SYM_OPT(name) \
-  if (!name) \
-  { \
-    PAL_ReportError("could not load symbol '%s'", #name); \
-    return 1; \
+  // before trying to run mono_main, try to call monovm_initialize
+  if (try_init_monovm(argv[1], monoHnd) == MONOVM_Error)
+  {
+    // this Mono is MonoVM, but it returned an error from monovm_initialize. Exit.
+    return 1;
   }
 
   LOAD_SYM_OPT(mono_main);
-
   if (mono_main)
   {
     // we have mono_main, pass off to that
